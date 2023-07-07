231aeae8c3854f5be25060779b2531ae52e6e491
mandars-icpl <mandars@infopercept.com>
2023-07-07 15:55:13 +0530

    role based authentication completed

--- src/php/auth.php
+++ src/php/auth.php
@@ -1,6 +1,6 @@
 <?php
 
-function authorize() {
+$role="default";
     ?>
     <script src="http://localhost:8000/js/keycloak.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/js-cookie@beta/dist/js.cookie.min.js"></script>
@@ -16,11 +16,28 @@ function authorize() {
         function logout() {
             keycloak.logout();
         }
+        function hasRole(role) {
+            return keycloak.hasRealmRole(role);
+        }
 
         keycloak.init(initOptions).success(function(authenticated) {
             if (authenticated) {
                 // User is logged in
                 Cookies.set('token', keycloak.token);
+                console.log(keycloak.hasRealmRole("quiz_creator"));
+                console.log(keycloak.hasRealmRole("quiz_approver"));
+                
+                if(keycloak.hasRealmRole("quiz_creator")){
+                    console.log();
+                    Cookies.set('role', "quiz_creator");
+                    //$role="quiz_creator";
+                    //location.href = "dashboard.php?role=quiz_creator";
+                }
+                else if(keycloak.hasRealmRole("quiz_approver")){
+                    Cookies.set('role', "quiz_approver");
+                    //$role="quiz_approver";
+                    //location.href = "dashboard.php?role=quiz_approver";
+                }
             } else {
                 // User is not logged in, redirect to login page
                 console.log("Redirect to login page ");
@@ -29,11 +46,4 @@ function authorize() {
         }).error(function() {
             console.log('Init Error');
         });
-        function hasRole(roles){
-            const hasRole = (roles) => roles.some((role) => keycloak.hasRealmRole(role));
-            return hasRole;
-        }
     </script>
-    <?php
-}
-?>
--- src/php/dashboard.php
+++ src/php/dashboard.php
@@ -1,25 +1,6 @@
 <?php
 require_once 'auth.php';
-authorize();
-echo "dashboard";
-?>
 
-<html>
-<head>
-    <title>Dashboard</title>
-</head>
-<body>
-    <h1>Welcome to the Dashboard</h1>
-    <script type="text/javascript">
-        if (hasRole(['quiz_creator'])) {
-            document.write("Role is Quiz Creator");
-        } 
-        if (hasRole(['quiz_approver'])) {
-            document.write("Role is Quiz Approver");
-        } 
-        else {
-            console.log("No Quiz Role found");
-        }
-    </script>
-</body>
-</html>
+print_r($_COOKIE);
+
+?>
